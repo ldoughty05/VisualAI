3ba19201a143368edeabd64c3c395a63e47f5adf
LionBub <luke.doughty@me.com>
2022-06-02 16:27:12 -0500

    test

---
+++ main.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from tkinter import ttk
+
+HEIGHT = 600
+WIDTH = 1000
+COL_A = '#36383a'
+COL_B = '#2b2b2b'
+COL_C = '#3c839d'
+
+rclickCanvasPos = (0, 0)  # global variables like this are discouraged. Make it into a class and save as instance?
+
+
+def pan_start(event):
+    canvas.scan_mark(event.x, event.y)
+
+
+def pan_move(event):
+    canvas.scan_dragto(event.x, event.y, gain=1)
+
+
+def do_popupMenu(event):
+    try:
+        m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+        global rclickCanvasPos
+        rclickCanvasPos = (canvas.canvasx(event.x), canvas.canvasy(event.y))
+
+
+def createGridLines(sizeX, sizeY, gridSpacing):
+    for x in range(int(sizeX / gridSpacing)):  # vertical lines
+        line = canvas.create_line(x * gridSpacing, 0, x * gridSpacing, sizeY, width=1, fill=COL_C)
+    for y in range(int(sizeY / gridSpacing)):  # vertical lines
+        line = canvas.create_line(0, y * gridSpacing, sizeX, y * gridSpacing, width=1, fill=COL_C)
+
+
+def addCircle(radius=25):
+    oval = canvas.create_oval(rclickCanvasPos[0] - radius, rclickCanvasPos[1] - radius,
+                              rclickCanvasPos[0] + radius, rclickCanvasPos[1] + radius,
+                              fill='green', outline='darkgreen', width=2)
+
+
+root = tk.Tk()
+root.title('Visual AI')
+root.geometry(f"{WIDTH}x{HEIGHT}")
+root.update_idletasks()  # strangely necessary to use winfo
+
+toolbar = tk.Frame(root, bg=COL_A)
+toolbar.place(relwidth=1, relheight=0.1)
+
+# panels
+panelA = tk.PanedWindow(root, bd=1, relief='raised', bg='gray', sashwidth=2, sashrelief='raised')
+panelA.place(relheight=0.9, relwidth=1, rely=0.1)
+inspector = tk.Frame(panelA, bg=COL_A)
+panelA.add(inspector, width=root.winfo_width() / 3)
+panelB = tk.PanedWindow(panelA, orient='vertical', bd=0, relief='sunken', bg='gray', sashwidth=2, sashrelief='raised')
+panelA.add(panelB)
+canvas = tk.Canvas(panelB, bg=COL_B)
+panelB.add(canvas, height=HEIGHT * 7 / 10)
+shelf = tk.Frame(panelB, bg=COL_A)
+panelB.add(shelf)
+
+# ---CANVAS----------------------------------------------------------
+# pan and scroll
+scrollbarX = tk.Scrollbar(canvas, orient='horizontal', command=canvas.xview)
+scrollbarY = tk.Scrollbar(canvas, orient='vertical', command=canvas.yview)
+# scrollbarX.pack(side="bottom", fill="x")
+# scrollbarY.pack(side='right', fill='y')
+canvas.configure(scrollregion=(0, 0, 2000, 1500), xscrollcommand=scrollbarX.set, yscrollcommand=scrollbarY.set)
+canvas.yview_moveto('0.5')
+canvas.xview_moveto('0.5')
+
+
+canvas.bind('<ButtonPress-2>', pan_start)
+canvas.bind('<B2-Motion>', pan_move)
+# right click menu
+m = tk.Menu(root, tearoff=0, bg=COL_A, fg='white', activebackground=COL_C)
+addmenu = tk.Menu(root, tearoff=0, bg=COL_A, fg='white', activebackground=COL_C)
+m.add_cascade(label='Add', menu=addmenu)
+m.add_separator()
+m.add_command(label="Cut")
+m.add_command(label="Copy")
+m.add_command(label="Paste")
+m.add_command(label="Reload")
+
+addmenu.add_command(label='Circle', command=addCircle)
+
+
+canvas.bind("<Button-3>", do_popupMenu)
+# grid
+
+
+createGridLines(2000, 1500, 100)
+coord = 10, 50, 240, 210
+arc = canvas.create_arc(coord, start=0, extent=150, fill="red")
+
+# labels
+toolbar_label = tk.Label(toolbar, text="Toolbar", bg='red')
+toolbar_label.pack()
+inspector_label = tk.Label(inspector, text="Inspector", bg='yellow')
+inspector_label.pack()
+canvas_label = tk.Label(canvas, text="Canvas", bg='green')
+canvas_label.pack()
+shelf_label = tk.Label(shelf, text="Shelf", bg='orange')
+shelf_label.pack()
+
+root.mainloop()
